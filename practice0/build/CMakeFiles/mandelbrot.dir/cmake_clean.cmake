e332458bf1f6b23ba290678eb08e0e31767b9fbf
PTaibo <p.taibo@udc.es>
2023-02-02 23:42:14 +0100

    Add CMake configuration

---
+++ practice0/build/CMakeFiles/mandelbrot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mandelbrot.dir/rational_pointer.c.o"
+  "CMakeFiles/mandelbrot.dir/rational_struct.c.o"
+  "mandelbrot"
+  "mandelbrot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mandelbrot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ practice0/build/CMakeFiles/rational.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rational.dir/main.c.o"
+  "CMakeFiles/rational.dir/rational_struct.c.o"
+  "rational"
+  "rational.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rational.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
