a40cd1378f54a9bd508e80872dc18eefaede0c2a
siyunesweek <siyuanhe24@gmail.com>
2023-03-11 10:42:35 +0100

    addContestant added

--- practice1/mainFunctions.h
+++ practice1/mainFunctions.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include "static_list.h"
 
-void addContestant(tList participants, char* participantName, char* EUParticipant);
+void addContestant(tList* participants, char* participantName, char* EUParticipant);
 void disqualifyContestant (char* participantName, tList participants, int* nullVotes);
 
 #endif
