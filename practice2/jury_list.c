fdf5fa3dded679ef14122d855cb13e3b7d2759fd
PTaibo <p.taibo@udc.es>
2023-03-30 11:21:34 +0200

    Add base files for practice 2

---
+++ practice2/jury_list.c
@@ -0,0 +1,13 @@
+/*
+ * TITLE: PROGRAMMING II LABS
+ * SUBTITLE: Practical 2
+ * AUTHOR 1: ***************************** LOGIN 1: **********
+ * AUTHOR 2: ***************************** LOGIN 2: **********
+ * GROUP: *.*
+ * DATE: ** / ** / **
+ */
+
+#include "jury_list.h"
+#include "participant_list.h"
+
+/* Write your code here... */
---
+++ practice2/jury_list.h
@@ -0,0 +1,18 @@
+/*
+ * TITLE: PROGRAMMING II LABS
+ * SUBTITLE: Practical 2
+ * AUTHOR 1: ***************************** LOGIN 1: **********
+ * AUTHOR 2: ***************************** LOGIN 2: **********
+ * GROUP: *.*
+ * DATE: ** / ** / **
+ */
+
+#ifndef JURY_LIST_H
+#define JURY_LIST_H
+
+#include "types.h"
+#include "participant_list.h"
+
+/* Write your code here... */
+
+#endif
