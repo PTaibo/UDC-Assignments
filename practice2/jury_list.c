19924e85d01a9f375732fbaf665f217d6bbc08fd
PTaibo <p.taibo@udc.es>
2023-03-30 19:48:41 +0200

    Create jury list implementation

--- practice2/jury_list.c
+++ practice2/jury_list.c
@@ -1,13 +1,129 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 2
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 1: Siyuan He            LOGIN 1: siyuan.he
+ * AUTHOR 2: Paula Taibo Suárez   LOGIN 2: p.taibo
+ * GROUP: 6.1
+ * DATE: 14 / 04 / 23
  */
+
+#include <string.h>
+#include <stdio.h>
 
 #include "jury_list.h"
 #include "participant_list.h"
 
-/* Write your code here... */
+#define UNUSED __attribute__((unused)) //Gets rid of unused parameter warnings
+
+void createEmptyListJ (tListJ* newList)
+{
+    newList->p = NULLJ;
+}
+
+bool isEmptyListJ(tListJ list)
+{
+    return list.p == NULLJ;
+}
+
+tPosJ firstJ(UNUSED tListJ list)
+{
+    return 0;
+}
+
+tPosJ lastJ (tListJ list)
+{
+    return list.p;
+}
+
+tPosJ nextJ (tPosJ pos, tListJ list)
+{
+    if (pos == list.p)
+        return NULLJ;
+    
+    return ++pos;
+}
+
+tPosJ previousJ (tPosJ pos, UNUSED tListJ list)
+{
+    return --pos;
+}
+
+bool insertItemJ (tItemJ item, tListJ* list)
+{
+    if (list->p + 1 == MAX_LIST)
+        return false;
+
+    list->p++;
+
+    //Case 1: empty list
+    if (!list->p){
+        list->jury[0] = item;
+        return true;
+    }
+
+    //Case 2: goes in the middle/end of the list
+    int i = list->p;
+
+    for (i = list->p; i > 0; i--){
+        if (strcmp(item.juryName, list->jury[i-1].juryName) > 0){
+            list->jury[i] = item;
+            return true;
+        }
+        list->jury[i] = list->jury[i-1];
+    }
+    
+    //Case 3: goes at the beginning of the list
+    list->jury[0] = item;
+    return true;
+}
+
+void deleteAtPositionJ (tPosJ pos, tListJ* list)
+{
+    //Case 1: empty list
+    if (list->p == NULLJ)
+        return;
+
+    //Case 2: delete last element
+    if (pos == list->p){
+        list->p--;
+        return;
+    }
+
+    //Case 3: element in the middle or at the start
+    for (int i = pos; i < list->p; i++){
+        list->jury[i] = list->jury[i+1];
+    }
+    list->p--;
+}
+
+tItemJ getItemJ (tPosJ pos, tListJ list)
+{
+    return list.jury[pos];
+}
+
+void updateItemJ (tItemJ item, tPosJ pos, tListJ* list)
+{
+    list->jury[pos] = item;
+}
+
+tPosJ findItemJ (tJuryName judge, tListJ list)
+{
+    //Binary search
+    int l = -1;
+    int r = list.p;
+
+    while (r > l + 1){
+        int m = (l+r) / 2;
+
+        if (strcmp(list.jury[m].juryName, judge) < 0)
+            l = m;
+        else
+            r = m;
+    }
+
+    //Check if the element was found
+    if (strcmp(list.jury[r].juryName, judge) != 0)
+        return NULLJ;
+
+    return r;
+}
--- practice2/jury_list.h
+++ practice2/jury_list.h
@@ -1,10 +1,10 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 2
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 1: Siyuan He            LOGIN 1: siyuan.he
+ * AUTHOR 2: Paula Taibo Suárez   LOGIN 2: p.taibo
+ * GROUP: 6.1
+ * DATE: 14 / 04 / 23
  */
 
 #ifndef JURY_LIST_H
@@ -13,6 +13,124 @@
 #include "types.h"
 #include "participant_list.h"
 
-/* Write your code here... */
+#define MAX_LIST 1000
+#define NULLJ -1
+
+//REMOVE WHEN SIYUAN ADDES HIS THINGIE
+typedef int* tListP;
+
+typedef struct {
+    tJuryName juryName;
+    tNumVotes totalVoters;
+    tNumVotes validVotes;
+    tNumVotes nullVotes;
+    tListP participantList;
+} tItemJ;
+
+typedef struct {
+    int p;
+    tItemJ jury[MAX_LIST];
+} tListJ;
+
+typedef int tPosJ;
+
+void createEmptyListJ (tListJ* newList);
+/*
+    Goal:create an empty list
+    Inputs: a pointer to a list (tList)
+    Outputs:NONE
+    Preconditions:NONE
+    Postconditions:The list is initialized and has no elements
+*/
+
+bool isEmptyListJ(tListJ list);
+/*
+    Goal:determines whether the list is empty or not
+    Inputs: a list (tList)
+    Outputs: if it is empty true else false
+    Preconditions:the list is initialized
+    Postconditions:NONE
+*/
+
+tPosJ firstJ(tListJ list);
+/*
+    Goal:returns the position of the first element of the list
+    Inputs: a list (tList)
+    Outputs: an integer of the position of the fist element
+    Preconditions:the list is initialized and not empty
+    Postconditions:NONE
+*/
+
+tPosJ lastJ (tListJ list);
+/*
+    Goal:returns the position of the last element of the list
+    Inputs: a list (tList)
+    Outputs: an integer of the position of the last element
+    Preconditions:the list is initialized and not empty
+    Postconditions:NONE
+*/
+
+tPosJ nextJ (tPosJ pos, tListJ list);
+/*
+    Goal:returns the following position in the list in respect of the indicated position
+    Inputs: a list (tList) and a position
+    Outputs: The next position of the list or LNULL if it has no next element
+    Preconditions:the list is initialized and the indicated position is a valid position in the list
+    Postconditions:NONE
+*/
+
+tPosJ previousJ (tPosJ pos, tListJ list);
+/*
+    Goal:returns the previous postion in the list in respect of the indicated position
+    Inputs: a list (tList) and a position
+    Outputs: the previous position of the list or LNULL if it has no previous element
+    Preconditions:the list is initialized and the indicated position is a valid position in the list
+    Postconditions:NONE
+*/
+
+bool insertItemJ (tItemJ item, tListJ* list);
+/*
+    Goal: inserts the item in the list, such that the list remains ordered
+    Inputs: the item (tItemJ) and the list in which to insert it (tListJ)
+    Outputs: true if the function was succesfull and false otherwise
+    Preconditions: the list is initialized
+    Postconditions: the positions of the elements following the inserted one are changed
+*/
+
+void deleteAtPositionJ (tPosJ pos, tListJ* list);
+/*
+    Goal: deletes the element at the given position from the list
+    Inputs: the position (tPosJ) and the list with the item to remove (tListJ)
+    Outputs: NONE
+    Preconditions: the list is initialized, the position is valid and the participantList is empty
+    Postconditions: the positions of the elements following the deleted one are changed 
+*/
+
+tItemJ getItemJ (tPosJ pos, tListJ list);
+/*
+    Goal:returns the content of the element at the indicated position
+    Inputs: a list (tList) and a position
+    Outputs: the element of data item type at the indicated position
+    Preconditions:the list is initialized and the indicated position is a valid position in the list
+    Postconditions:NONE
+*/
+
+void updateItemJ (tItemJ item, tPosJ pos, tListJ* list);
+/*
+    Goal: Modifies the content of the element at the indicated position
+    Inputs: a pointer to a list (tList), a item of data item type and a position
+    Outputs:NONE
+    Preconditions:the list is initialized and the indicated position is a valid position in the list
+    Postconditions:The order of the elements in the list has not been modified
+*/
+
+tPosJ findItemJ (tJuryName judge, tListJ list);
+/*
+    Goal:Returns the position of the first element in the list whose jury name matches the given one
+    Inputs: a jury name and a list
+    Outputs: an integer of the position where the name match at first time or LNULL if there is no such element
+    Preconditions:the list is initialized.
+    Postconditions:NONE
+*/
 
 #endif
