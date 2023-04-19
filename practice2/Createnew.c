7111aa8bf5eb1480c292bf9c2ea6d3246b3123e4
siyunesweek <siyuanhe24@gmail.com>
2023-04-19 18:52:57 +0200

    didnt save the Create file

--- practice2/Createnew.c
+++ practice2/Createnew.c
@@ -46,19 +46,20 @@ void addParticipant(char *juryName, char *ParticipantName, char *EUParticipant, 
         Postconditions: NONE 
     */
     if(isEmptyListJ(*juryVotesList)==true){//we check if there exits juries
-        printf("+Error: New not possible\n");
+        printf("+ Error: New not possible\n");
         return;
     }
     tPosJ pos;
     pos=findItemJ(juryName,*juryVotesList);
     if (pos==NULLJ){//Look if the judge exit
-        printf("+Error: New not possible\n");
+        printf("+ Error: New not possible\n");
         return;
     }
     tItemJ juri;
     juri=getItemJ(pos,*juryVotesList);
-    if(findItemP(ParticipantName,juri.participantList)!=NULLP){//Look if exits
-        printf("+Error: New not possible\n");
+    tPosP ppart=findItemP(ParticipantName,juri.participantList);
+    if(ppart!=NULLP){//Look if exits
+        printf("+ sdfswaError: New not possible\n");//el error me sale aquí
         return;
     }
     tItemP temp;//I create the participant
@@ -68,53 +69,11 @@ void addParticipant(char *juryName, char *ParticipantName, char *EUParticipant, 
     if (strcmp(EUParticipant,"eu")==0)
         temp.EUParticipant=true;
     if(insertItemP(temp,juri.participantList)==false){
-        printf("+Error: New not possible\n");
+        printf("+ Error: New not possible\n");
         return;
     }
     updateItemJ(juri,pos,juryVotesList);
-    printf("*New: jury %s participant %s location %s\n",juryName,temp.participantName,EUParticipant);
+    printf("* New: jury %s participant %s location %s\n",juryName,temp.participantName,EUParticipant);
         return;
 }
-
-void addParticipant(char *juryName, char *ParticipantName, char *EUParticipant, tListJ *juryVotesList)
-{
-    /*
-        Goal: Add participant to the jury 
-        Input: jury name, participant name, if it is er or non-eu and the jury list
-        Output: NONE
-        Preconditions: jury's list is initialized, valid names and a valid eu value
-        Postconditions: NONE 
-    */
-    if(isEmptyListJ(*juryVotesList)==true){//we check if there exits juries
-        printf("+Error: New not possible\n");
-        return;
-    }
-    tPosJ pos;
-    pos=findItemJ(juryName,*juryVotesList);
-    if (pos==NULLJ){//Look if the judge exit
-        printf("+Error: New not possible\n");
-        return;
-    }
-    tItemJ jury;
-    jury=getItemJ(pos,*juryVotesList);
-    if(findItemP(ParticipantName,jury.participantList)!=NULLP){//Look if exits
-        tItemP temp;//I create the participant
-        strcpy(temp.participantName,ParticipantName);
-        temp.numVotes=0;
-        temp.EUParticipant=false;
-        if (strcmp(EUParticipant,"eu")==0)
-            temp.EUParticipant=true;
-        if(insertItemP(temp,jury.participantList)==false){
-            printf("+Error: New not possible\n");
-            return;
-        }
-        updateItemJ(jury,pos,juryVotesList);
-        printf("*New: jury %s participant %s location %s\n",juryName,temp.participantName,EUParticipant);
-            return;
-
-    }
-    printf("+Error: New not possible\n");
-        return;    
-}
-
    
