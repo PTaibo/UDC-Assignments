cf2a402014e5cbc37243f171fec22b88eeb298d8
siyunesweek <siyuanhe24@gmail.com>
2023-09-18 09:47:38 +0200

    Base code
    The creation of the universe

---
+++ algorithms/p1/algorithms_p1_Paula_Siyuan.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+
+int maxSubSum1(int v[], int n) {
+int i, j;
+long thisSum, maxSum = 0;
+for (i = 0; i < n; i++) {
+thisSum = 0;
+for (j = i; j < n; j++) {
+thisSum += v[j];
+if (thisSum > maxSum) {
+maxSum = thisSum;
+}
+}
+}
+return maxSum;
+}
+int maxSubSum2(int v[], int n) {
+/* ... */
+}
+void print_array(int v[], int n){
+/* ... */
+}
+void test1() {
+/* ... */
+}
+void test2() {
+int i, a, b;
+int v[9];
+printf("test\n");
+printf("%33s%15s%15s\n", "", "maxSubSum1", "maxSubSum2");
+for (i=0; i<10; i++) {
+random_init(v, 9);
+print_array(v, 9);
+a = maxSubSum1(v, 9);
+b = maxSubSum2(v, 9);
+printf("%15d%15d\n", a, b);
+}
+}
+int main() {
+init_seed();
+test1();
+test2();
+return 0;
+}
