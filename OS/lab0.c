f69b8d2abc458f0040e7ebc1338013ba43ef0d48
PTaibo <p.taibo@udc.es>
2023-09-25 16:18:39 +0200

    Create file distribution for project

--- OS/lab0.c
+++ OS/lab0.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/* #include <sys/types.h> */
+#include <unistd.h>
 
 // Define maxline instead of putting 200 directly
 #define MAXLINE 200
+
+struct CMD {
+    char* name;
+    void (*funct)(char**);
+};
 
 int trocearCadena(char* cadena, char* trozos[])
 {
@@ -25,20 +32,47 @@ void Cmd_authors(char *tr[])
 {
   if (tr[0] == NULL)
     printf("logins and names...\n");
-  if (!strcmp(tr, "-l") || !strcmp(tr, "-n"))
+  if (!strcmp(tr[0], "-l") || !strcmp(tr[0], "-n"))
       printf("logins.....\n");
   else
         printf("logins and names...\n");
+}
+
+void Cmd_pid (char *tr[])
+{
+    printf("Shell0s pid is %d\n", getpid());
+}
+
+/* char* joinStr (char sl, char* s2) */
+/* { */
+/*     char* p; */
+/*     p = malloc(strlen(s1)+strlen(s2)+1); // For '\0' (not in string length) */
+
+/*     char p[MAX]; */
+/*     ... */
+/*     return p; //WRONG!!! Because p is local so you loose it in stack when you go back */
+/*     //Make p static to solve problem */
+/* } */
+
+struct CMD valid_commands[]={
+    {"authors", Cmd_authors},
+    {"quit", Cmd_quit},
+    {"exit", Cmd_quit},
+    {"bye", Cmd_quit},
+    {"pid", Cmd_pid},
+    {NULL, NULL}
+};
+
 
 void procesarComando(char * tr[])
 {
-  if (tr[0] == NULL){
-    return;
-  if (!strcmp(tr, "authors"))
-    Cmd_authors(tr+1);
-  else if (!strcmp(tr, "quit") || !strcmp(tr, "bye") || !strcmp(tr, "exit"))
-    Cmd_quit();
-  else
+    int i;
+    for (i=0; valid_commands[i].name != NULL; i++){
+        if (!strcmp(tr[0], valid_commands[i].name)) {
+                (*valid_commands[i].funct)(tr+1);
+                return;
+        }
+    }
     printf("Command not found\n");
 }
 
