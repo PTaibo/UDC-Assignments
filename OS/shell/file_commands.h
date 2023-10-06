5194b36b2629e210ad0cab5b3df1eb4056cac005
PTaibo <p.taibo@udc.es>
2023-10-06 10:07:40 +0200

    Fix variable name typo

--- OS/shell/file_commands.h
+++ OS/shell/file_commands.h
@@ -4,6 +4,6 @@ void init_file_list();
 int check_file_commands(int paramN, char* command[]);
 
 void cmd_open (int paramN, char* params[]);
-void cmd_close (int pramN, char* params[]);
+void cmd_close (int paramN, char* params[]);
 void cmd_dup (int paramN, char* params[]);
-void cmd_listopen(int paramN, char* params[]);
+void cmd_listopen (int paramN, char* params[]);
