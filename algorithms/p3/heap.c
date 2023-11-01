fef1466e80426ff80dda9359089d5a94a7ae53fa
siyunesweek <siyuanhe24@gmail.com>
2023-11-01 02:18:00 +0100

    Alg: heap list implemented

--- algorithms/p3/heap.c
+++ algorithms/p3/heap.c
@@ -6,57 +6,28 @@ GROUP:6.1                     DATE:23/10/2023
 */
 
 #include "heap.h"
-#include <stdbool.h>
-#include <stdio.h>
 
-/*void initialize_heap(pheap h)
-{
-    h->last=0;
-}
 
-bool isEmptyHeap(pheap h)
-{
-    return h->last=0;
-}
+void initialize_heap(pheap h){
 
-order(pheap h)
-
-bool insertH(int a, pheap h)
-{
-    if (h->last=SZ){
-        return 0;
+    if ((h = (struct heap *)malloc(sizeof(struct heap))) == NULL) {        
+        return;
     }
-    else{
-        h->last += 1;
-        h->vector[h->last] = a;
-        order();
-    }
-    
 
 }
+
+
 
 void create_heap(int a[], int n, pheap h)
 {
-    bool insert=true;
 
     initialize_heap(h);
 
-    for (int i = 0; i < n, insert != false; i++){
-        insert=insertH(a[i],h);
-    }
-    
-    if(!insert){
-        printf("Heap created incorrectly");
-    }
-
-}*/
-
-void create_heap(int a[], int n, pheap h)
-{
     h->last=n-1;
     for (int i = 0; i < n; i++){
         h->vector[i]=a[i];
     }
+
 }
 
 int remove_min(pheap h)
@@ -66,30 +37,36 @@ int remove_min(pheap h)
         return -1;
     }
 
-    int min = h->vector[0];//we start in the first
+    //we keep the first one
+    int min = h->vector[0];
     int pos = 0;
 
     //iterate through the list
     for (int i = 1; i <= h->last; i++){
 
+        //we check if it is smaller
         if (h->vector[i] < min){//we check
-            min = h->vector;
+            min = h->vector[i];
             pos = i;//we keep the position
         }
+
     }
 
     //we delete it from the list
     for (int i = pos; i < h->last; i++){
         h->vector[i] = h-> vector [i+1];
     }
+
     h->last--;
 
     return min;
+
 }
 
 void heap_sort (int a[], int n)
 {
-    pheap h;
+    pheap h = (struct heap *)malloc(sizeof(struct heap));
+
     create_heap (a, n, h);
     for (int i = 0; i < n; i++){
         a[i] = remove_min(h);
