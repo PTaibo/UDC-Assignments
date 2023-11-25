befaed5672aec1569025a5857860e35e39ca9d08
siyunesweek <siyuanhe24@gmail.com>
2023-11-25 03:03:23 +0100

    Alg: dijkstra implemented

--- algorithms/p4/algorithms_p4_Paula_Siyuan.c
+++ algorithms/p4/algorithms_p4_Paula_Siyuan.c
@@ -2,22 +2,47 @@
 
 typedef int **matrix;
 
+int minimindistance (int *distances, int *unvisited, int n){
+
+    int minIndex = -1;
+    int minValue = INT_MAX;
+
+    for (int i = 0; i < n; i++) {
+        if (unvisited[i] && distances[i] < minValue) {
+            minValue = distances[i];
+            minIndex = i;
+        }
+    }
+
+    return minIndex;
+}
+
 
 void dijkstra(matrix graph, matrix distances, int sz)
 {
-    int n, i, j, min, v = 0;
+    int n, min, v = 0;
     int *unvisited = malloc(sz * sizeof(int));
     for (n = 0; n < sz; n++)
     {
-        for (i = 0; i < sz; i++)
+        for (int i = 0; i < sz; i++)
         {
             unvisited[i] = 1;
             distances[n][i] = graph[n][i];
         }
         unvisited[n] = 0;
-        /*
-        ...
-        */
+
+        for (int i = 0; i < n -2; i++){
+            int v = minimindistance(distances[n], unvisited, n);
+
+            unvisited[v] = 0;
+
+             for (int j = 0; j < n; j++) {
+                if (unvisited[j] && Distances[n][j] > Distancesn[n][j] + M[v][j]) {
+                    Distances[n][j] = Distances[n][v] + M[v][j];
+                }   
+
+            }
+        }
     }
     free(unvisited);
 }
